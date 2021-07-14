05a48f4a383013400436f41a7a8d187c3b4210c2
Ibrahim Waheed <Ibrahimwaheed@hotmail.co.uk>
2021-07-14 10:31:59 +0100

    code a better solution however it might be wrong as i am storing the string value in a dic/hash  map

--- Puzzle Questions/reccuringChar.py
+++ Puzzle Questions/reccuringChar.py
@@ -4,7 +4,7 @@
 #and if u have GHAH then H 
 
 
-def recurringCharD(string):
+def recurringCharLoop(string):
     for index in range(len(string)):
         for char in range(index):
             if string[char]  == string[index]:
@@ -14,4 +14,22 @@ def recurringCharD(string):
 
 
 
-print(recurringCharD("ABCSBA"))
+
+
+# might be wrong but store the string in dic/hash map should help and make it better I think
+# so if we have A B C then dic will go as following A:1 B:1 C:1 because of key must be unique and value can be anything if it see A B C A  then in the 4 interation
+# it would return the character via if condition   
+
+
+def recurringCharHasHMap(string):
+    countTheChars  = {}
+    for character in string:
+        if character in countTheChars:
+            return character
+        countTheChars[character] = 1 
+    return None
+
+
+print(recurringCharLoop("ABBA"))
+
+print(recurringCharHasHMap("ABBA"))
