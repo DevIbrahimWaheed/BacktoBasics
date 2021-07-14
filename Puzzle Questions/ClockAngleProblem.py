23084de459c01ce9fe160befcd78ec5a1ab0d1ed
Ibrahim Waheed <Ibrahimwaheed@hotmail.co.uk>
2021-07-14 11:15:30 +0100

    done the clock angle problem  first i did a hard coded if statement which was .... bad then found out that it was a math issue!

--- Puzzle Questions/ClockAngleProblem.py
+++ Puzzle Questions/ClockAngleProblem.py
@@ -1,9 +1,20 @@
 
 
 
-def angle(h,mm):
-   return (h,mm)
+# okay what do we know 360 in a circle 
+# so the we create a forumla of analog 12 
 
 
-print(angle(2,55))
 
+
+def timetoAngle(H,M):
+   hours = (H*360) // 12 + (M*360) // (12 * 60)
+   minutes  = (M  * 360) // 60
+   getAngle = abs(hours - minutes)
+   if getAngle > 180:
+      getAngle = 360 - getAngle
+   return getAngle
+
+
+
+print(timetoAngle(5,30))
